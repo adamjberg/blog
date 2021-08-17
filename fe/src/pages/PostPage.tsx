4240600b84af65e79723a3ea126e097f1bfb7943
Adam Berg <adam@xyzdigital.com>
2021-08-16 19:03:22 -0700

    Add basic side by side markdown editting

--- fe/src/pages/PostPage.tsx
+++ fe/src/pages/PostPage.tsx
@@ -1,31 +1,83 @@
 import React from "react";
 import { useEffect } from "react";
-import Quill from "quill";
+import { useState } from "react";
+import { createRef } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+
+
+import "./PostPage.scss";
 
 export const PostPage: React.FC = () => {
+  const textAreaRef = createRef<HTMLTextAreaElement>();
+
   const post = {
     title: "Hello World",
     body: "This is my first post",
   };
 
+  const [body, setBody] = useState(post.body);
+
   useEffect(() => {
-    new Quill("#editor", {
-      theme: "snow",
-      modules: {
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          ['image', 'code-block']
-        ]
-      },
-    })
-  }, [])
+    const textArea = textAreaRef.current;
+    if (!textArea) {
+      return;
+    }
+
+    const handleDrop = async (e: any) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.dataTransfer?.files && e.dataTransfer.files.length > 0) {
+        const data = await handleUploadImage(e.dataTransfer.files[0]);
+        textArea.setRangeText(`![](${data.link})`);
+
+        const nextSelection = textArea.selectionStart + 2;
+        textArea.setSelectionRange(nextSelection, nextSelection);
+
+        e.dataTransfer.clearData();
+      }
+    };
+
+    textArea.addEventListener("drop", handleDrop);
+
+    return () => {
+      textArea.removeEventListener("drop", handleDrop);
+    };
+  }, [textAreaRef]);
+
+  function handleTextChanged(e: any) {
+    setBody(e.currentTarget.value)
+  }
+
+  async function handleUploadImage(file: File) {
+    let formData = new FormData();
+
+    formData.append("image", file);
+
+    const res = await fetch("/api/images", {
+      method: "POST",
+      body: formData,
+    });
+
+    const jsonData = await res.json();
+    return jsonData.data;
+  }
 
   return (
     <div className="post-page">
       <div className="container">
         <h1>{post.title}</h1>
-        <div id="editor" className="post-page__body">{post.body}</div>
+      </div>
+      <div className="split-view">
+        <textarea
+          ref={textAreaRef}
+          className="post-page__body"
+          onChange={handleTextChanged}
+          value={body}
+          rows={40}
+        ></textarea>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{body}</ReactMarkdown>
       </div>
     </div>
   );
